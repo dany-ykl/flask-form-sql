3724d85fbf630583f10dadf7fa98d276e133ca8f
dany-ykl <dany.ykl3@yandex.ru>
2022-02-05 23:10:38 +0300

    Add class Insert, Update, Delete

--- TestTask/form.py
+++ TestTask/form.py
@@ -1,6 +1,7 @@
 from types import NoneType
 from flask import request
 from flask import Flask, render_template, redirect, url_for
+from sqlalchemy import delete
 
 import service
 
@@ -16,104 +17,24 @@ def index():
     conn.close()
     return render_template('index.html', datasources=datasources)
 
-@app.route('/insert/', methods=('GET', 'POST'))
-def form_flask_insert():
+@app.route('/work-with-db/', methods=('GET', 'POST'))
+def work_with_db():
+    """insert update delete"""
     conn = service.get_db_connection()
     cur = conn.cursor()
 
-    if request.method == "POST":
-        insert_db = service.InsertDB(conn, cur, request)
-        insert_db.insert()
+    if request.method == 'POST':
+        type_request = request.form['request_db']
+
+        if type_request == 'insert':
+            service.form_flask_insert(conn, cur, request)
+
+        if type_request == 'update':
+            service.form_flask_update(conn, cur, request)
+
+        if type_request == 'delete':
+            service.form_flask_delete(conn, cur, request)
+
         return redirect(url_for('index'))
-    return render_template('insert.html')
+    return render_template('work_with_db.html')
 
-def set_update_db(fields):
-    fields_not_none = []
-    update_command = []
-
-    for field, v in fields.items():
-        if v !="":
-            fields_not_none.append({field:v})
-    
-    for i in fields_not_none:
-        for key, value in i.items():
-            update_command.append(f"{key} = '{value}'")
-    req = ', '.join(update_command)
-    return req
-
-
-@app.route('/update/', methods=('GET', 'POST'))
-def form_flask_update():
-    conn = service.get_db_connection()
-    cur = conn.cursor()
-
-    if request.method == "POST":
-        data = service.featch_data_from_request(request)
-
-        fields = {
-            "description":data["description"],
-            "httpaddress":data['httpaddress'],
-            "apibaseurl":data["apibaseurl"],
-            "apischemeurl":data["apischemeurl"],
-            "marked":data["marked"]
-            }
-
-        req = set_update_db(fields)
-
-        if enumname == 'ApiKey':
-            authorizationjson = request.form['authorizationjson']
-            cur.execute(f"UPDATE datasources\
-                SET {req}\
-                WHERE authorizationjson = '{authorizationjson}'")
-
-            conn.commit()
-            cur.close()
-            conn.close()
-
-        if enumname == 'BearerToken':
-            autorizationtoken = request.form['autorizationtoken']
-            cur.execute(f"UPDATE datasources\
-                SET {req}\
-                WHERE autorizationtoken = '{autorizationtoken}'")
-
-            conn.commit()
-            cur.close()
-            conn.close()
-
-        if enumname == 'BasicAuth':
-            autorizationlogin = request.form['autorizationlogin']
-            autorizationpassword = request.form['autorizationpassword']
-            cur.execute(f"UPDATE datasources\
-                SET {req}\
-                WHERE autorizationpassword = '{autorizationpassword}' AND autorizationlogin = '{autorizationlogin}'")
-
-            conn.commit()
-            cur.close()
-            conn.close()
-        
-        return render_template('update.html')
-    return render_template('update.html')
-
-#@app.route('/delete/', methods=('GET', 'POST'))
-#def form_flask_delete():
-#    conn = get_db_connection()
-#    cur = conn.cursor()
-#
-#    if request.method == "POST":
-#        uuid_db = uuid.uuid4()
-#        description = request.form['description']
-#        enumname = request.form.get('enumname')
-#        httpaddress = request.form['httpaddress']
-#        apibaseurl = request.form['apibaseurl']
-#        apischemeurl = request.form['apischemeurl']
-#        marked = request.form.get('True')
-#        if type(marked) == str:
-#            marked = 'TRUE'
-#        else: marked = 'FALSE'
-#
-#        cur.execute(f"DELETE FROM datasources WHERE apischemeurl = '{apischemeurl}' AND description = '{description}' ")
-#        conn.commit()
-#        cur.close()
-#        conn.close()
-#        return render_template('delete.html')
-#    return render_template('delete.html')
--- TestTask/service.py
+++ TestTask/service.py
@@ -1,9 +1,7 @@
 import os
-from types import NoneType
 import uuid
 
 import psycopg2
-
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
@@ -40,7 +38,6 @@ def featch_data_from_request(request):
 
     return data
 
-
 class InsertDB:
 
     def __init__(self, conn, cur, request):
@@ -51,11 +48,11 @@ class InsertDB:
 
     def insert(self):
         if check_data(self.data):
-            return self.factory_auth()
+            return self._factory_auth()
         else:
             return None
 
-    def factory_auth(self):
+    def _factory_auth(self):
         if self.data["enumname"] == 'NoAuth':
             return self.insert_noauth()            
 
@@ -115,18 +112,100 @@ class InsertDB:
 
         commit_and_close(self.conn, self.cur)    
 
+class UpdateDB:
 
+    def __init__(self, conn, cur, request):
+        self.conn = conn
+        self.cur = cur
+        self.request = request
+        self.data = featch_data_from_request(self.request)
 
-def set_update_db(fields):
-    fields_not_none = []
-    update_command = []
+    def set_update_db(self):
+        fields_not_none = []
+        update_command = []
 
-    for field, v in fields.items():
-        if v !="":
-            fields_not_none.append({field:v})
+        fields = {
+            "description":self.data["description"],
+            "httpaddress":self.data['httpaddress'],
+            "apibaseurl":self.data["apibaseurl"],
+            "apischemeurl":self.data["apischemeurl"],
+            "marked":self.data["marked"]
+            }
     
-    for i in fields_not_none:
-        for key, value in i.items():
-            update_command.append(f"{key} = '{value}'")
-    req = ', '.join(update_command)
-    return req
+        for field, v in fields.items():
+            if v !="":
+                fields_not_none.append({field:v})
+        
+        for i in fields_not_none:
+            for key, value in i.items():
+                update_command.append(f"{key} = '{value}'")
+        req = ', '.join(update_command)
+    
+        return req
+
+    def update(self):
+        return self._factory_auth()
+
+    def _factory_auth(self):
+        if self.data["enumname"] == 'ApiKey':
+            return self.update_apikey()
+
+        if self.data["enumname"] == 'BearerToken':
+            return self.update_bearertoken()
+
+        if self.data["enumname"] == 'BasicAuth':
+            return self.update_basicauth()
+
+    def update_apikey(self):
+        authorizationjson = self.request.form['authorizationjson']
+        req = self.set_update_db()
+        self.cur.execute(f"UPDATE datasources\
+            SET {req}\
+            WHERE authorizationjson = '{authorizationjson}'")
+
+        commit_and_close(self.conn, self.cur)
+
+    def update_bearertoken(self):
+        autorizationtoken = self.request.form['autorizationtoken']
+        req = self.set_update_db()
+        self.cur.execute(f"UPDATE datasources\
+            SET {req}\
+            WHERE autorizationtoken = '{autorizationtoken}'")
+        
+        commit_and_close(self.conn, self.cur)
+
+    def update_basicauth(self):
+        autorizationlogin = self.request.form['autorizationlogin']
+        autorizationpassword = self.request.form['autorizationpassword']
+        req = self.set_update_db()
+        self.cur.execute(f"UPDATE datasources\
+            SET {req}\
+            WHERE autorizationpassword = '{autorizationpassword}' AND autorizationlogin = '{autorizationlogin}'")
+        
+        commit_and_close(self.conn, self.cur)
+
+class DeleteDB:
+
+
+    def __init__(self, conn, cur, request):
+        self.conn = conn
+        self.cur = cur
+        self.request = request
+        self.data = featch_data_from_request(self.request)
+
+    def delete_record(self):
+        self.cur.execute(f"DELETE FROM datasources WHERE\
+            apischemeurl = '{self.data['apischemeurl']}' AND description = '{self.data['description']}';")
+        commit_and_close(self.conn, self.cur)
+
+def form_flask_insert(conn, cur, request):
+    insert_db = InsertDB(conn, cur, request)
+    insert_db.insert()
+
+def form_flask_update(conn, cur, request):
+    update_db = UpdateDB(conn, cur, request)
+    update_db.update()
+
+def form_flask_delete(conn, cur, request):
+    delete_db = DeleteDB(conn, cur, request)
+    delete_db.delete_record()
